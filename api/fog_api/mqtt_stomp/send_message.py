c0547e611472fe437696e3eae8ca8623d3ca4dd0
juanmansano <juanmansano@gmail.com>
2023-09-25 23:38:08 -0300

    fix(send_message)

--- api/fog_api/mqtt_stomp/send_message.py
+++ api/fog_api/mqtt_stomp/send_message.py
@@ -5,6 +5,7 @@ from fog_api.models.atividades import Atividades
 
 def send_message(dispostivo_id, atividade_id):
     atividade = db_core.query(Atividades).filter(Atividades.id == atividade_id).first()
-    message = {"iluminancia": atividade.iluminancia}
+    message = f'{"iluminancia": {atividade.iluminancia}}'
+    print(message)
 
     broker_conn.publish(config.PUB_TOPIC, message, config.QOS)
