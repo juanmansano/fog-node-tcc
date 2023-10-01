de582c39aec4a8cb2bdfd8008df9fbb283c1c3fe
juanmansano <juanmansano@gmail.com>
2023-09-30 22:11:06 -0300

    retirada dos pritns

--- api/fog_api/mqtt_stomp/send_message.py
+++ api/fog_api/mqtt_stomp/send_message.py
@@ -14,7 +14,6 @@ def send_message(dispostivo_id, atividade_id):
     atividade = db_core.query(Atividades).filter(Atividades.id == atividade_id).first()
     message = {"atividade": atividade.nome, "iluminancia": atividade.iluminancia}
     message = json.dumps(message)
-    print(message)
 
     if config.PROTOCOL == 'stomp':
         broker_conn.send(body=str(message), destination=config.STOMP_PUB_TOPIC)
@@ -23,7 +22,6 @@ def send_message(dispostivo_id, atividade_id):
 
     while not answer:
         time.sleep(0.1)
-        print('esperando resposta')
 
     match_awnser = message_recived(dispostivo_id, atividade_id, answer)
 
