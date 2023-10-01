709b77b5485ec064d210053685699d041bfa3c6f
juanmansano <juanmansano@gmail.com>
2023-10-01 13:23:35 -0300

    ajustes json

--- api/fog_api/tasks/recived_message.py
+++ api/fog_api/tasks/recived_message.py
@@ -3,10 +3,13 @@
 from fog_api.tasks.new_device import adicionar_dispositivo
 from fog_api.tasks.change_status import check_return
 
+import json
 
 def message_recived(message):
     print("function message recived")
     print(message)
+    print(json.loads(message))
+    print(json.loads(message).get('ativar_owner'))
     if message["ativar_owner"]:
         adicionar_dispositivo(message)
     elif message["atualizado"]:
